b381c4b97ad9b158a47fd3a955453d36f6bdbac1
Soraya-j <21255@ecam.be>
2024-12-31 14:31:48 +0100

    detection open/close hand

--- test.py
+++ test.py
@@ -4,34 +4,54 @@ import mediapipe.python.solutions.drawing_utils as drawing
 #import mediapipe.python.solutions.drawing_styles as drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=False,max_num_hands=2,min_detection_confidence=0.5)
+screen_width = 1280
+screen_height = 960
+
+def detect_gesture_right(hand_landmarks):
+    print("fonction detect gesture")
+    finger_tips = [8, 12, 16, 20]  
+    finger_mcp = [6, 10, 14, 18]    
+
+    fingers_up = []
+    for tip, mcp in zip(finger_tips, finger_mcp):
+        if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[mcp].y:
+            fingers_up.append(True)  
+        else:
+            fingers_up.append(False)  
+
+    if all(fingers_up):  
+        print("Open hand")
+    elif not any(fingers_up):  
+        print("Close hand")
+    else:
+        print("unknow gesture")
 
 def detect_hand(frame):
     hands_detected = hands.process(frame)
     img_rgb = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     if hands_detected.multi_hand_landmarks:
         for landmarks in hands_detected.multi_hand_landmarks:
-
             handedness = hands_detected.multi_handedness[hands_detected.multi_hand_landmarks.index(landmarks)].classification[0].label
             if handedness == "Left":  
-                print('Une main est detectee: Left')
+                print('LEFT hand detected')
             else :
-                print('Une main est detectee: Right')
+                print('RIGHT hand detected')
+                detect_gesture_right(landmarks)
     else : 
-        print('Aucune main detectee')
+        print('No hands detected')
 
     return img_rgb
         
 
-def capture_video():        
+def capture_video(width, height):        
     cam = cv2.VideoCapture(0)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     while (True):
         ret, frame = cam.read()
         flip_frame = cv2.flip(frame,1) 
         img_bgr = cv2.cvtColor(flip_frame, cv2.COLOR_BGR2RGB)
         img_rgb = detect_hand(img_bgr)
-        
             # for hand_landmarks in hands_detected.multi_hand_landmarks:
             #     drawing.draw_landmarks(img_rgb, hand_landmarks, mp_hands.HAND_CONNECTIONS, drawing_styles.get_default_hand_landmarks_style(), drawing_styles.get_default_hand_connections_style())
         cv2.imshow('Detected',img_rgb)
@@ -40,4 +60,4 @@ def capture_video():
     cv2.destroyAllWindows()
     cam.release()
 
-capture_video()
+capture_video(screen_width, screen_height)
